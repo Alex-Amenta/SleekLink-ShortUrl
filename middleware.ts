ac9dbcf3c7391d186c37fd42dbcb79a1b4d6f7e8
Alex-Amenta <thekinggcat@gmail.com>
2025-01-28 18:57:33 -0300

    add protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,17 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { NextResponse } from "next/server";
 
 export const { auth: middleware } = NextAuth(authConfig);
+
+export default middleware((req) => {
+  const { nextUrl, auth } = req;
+
+  const isLoggedIn = !!auth?.user; // Verifica si el usuario está autenticado
+
+  if (!isLoggedIn && nextUrl.pathname.startsWith("/dashboard")) {
+    NextResponse.redirect(new URL("/auth/login", nextUrl.origin));
+  }
+
+  return NextResponse.next();
+});
